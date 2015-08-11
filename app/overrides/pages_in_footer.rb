4da7346dedde573462fb33984da9c0a0a9e0806f
Raul Popadineti <raulpopadineti@gmail.com>
2015-08-11 11:59:28 +0300

    refactor(overrides): add `:original` attribute to footer override; change position of static pages from `#footer-right` to `#footer-left`

--- app/overrides/pages_in_footer.rb
+++ app/overrides/pages_in_footer.rb
@@ -1,5 +1,6 @@
 Deface::Override.new(:virtual_path => "spree/shared/_footer",
                      :name => "pages_in_footer",
-                     :insert_bottom => "#footer-right",
+                     :insert_bottom => "#footer-left",
                      :partial => "spree/static_content/static_content_footer",
-                     :disabled => false)
+                     :disabled => false,
+                     :original => "81e1f27f30daed92a8861f408701fe9202bf52cf")
